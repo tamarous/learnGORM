e533a11d2a6e4e71fb64c3b3f06808d630fd59b2
Tamarous <hiwangzewei@qq.com>
2022-07-27 23:21:41 +0800

    ci: use viper for config

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 
@@ -8,7 +11,22 @@ import (
 )
 
 func main() {
-	dsn := "root:password@tcp(127.0.0.1:3306)/classicmodels?charset=utf8mb4&parseTime=True&loc=Local"
+
+	viper.SetConfigName("db")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./configs/")
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Printf("err is %v", err)
+	}
+
+	username := viper.GetString("MySQL.Username")
+	password := viper.GetString("MySQL.Password")
+	port := viper.GetInt("MySQL.Port")
+
+	fmt.Printf("username: %s, password: %s, port: %d\n", username, password, port)
+
+	dsn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:%d)/classicmodels?charset=utf8mb4&parseTime=True&loc=Local", username, password, port)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
